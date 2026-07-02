27bf69ac133f7c2c192c230297b94ba25c7dafbb
Fabian <fabian98cloud@gmail.com>
2026-07-01 18:06:15 -0600

    feat: add AKS cluster

--- main.tf
+++ main.tf
@@ -2,3 +2,24 @@ resource "azurerm_resource_group" "main" {
   name     = "rg-${var.project_name}-${var.environment}"
   location = var.location
 }
+
+resource "azurerm_kubernetes_cluster" "main" {
+  name                = "aks-${var.project_name}-${var.environment}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  dns_prefix          = "aks-${var.project_name}-${var.environment}"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2s_v7"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    environment = var.environment
+  }
+}
